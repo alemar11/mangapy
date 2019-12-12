8d7150d908a761d477bad0973e8875e5e46dbaf1
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-12 18:08:19 +0100

    --wip-- [skip ci]

--- manga/MangaFox.py
+++ manga/MangaFox.py
@@ -66,7 +66,32 @@ class MangaFoxChapter(Chapter):
         test_url = 'http://fanfox.net/manga/naruto/v01/c000/13.html'
         soup = get_page_soup(test_url)
         #soup.find('img', {'class': 'reader-main-img'})['src']
-        print(soup)
+
+        soup.findAll('script', {'type': 'text/javascript'})
+
+        '''
+        script text/javascript
+
+                var csshost = "http://static.fanfox.net/v201906282/mangafox/";        
+                var comicid = 11529;        
+                var chapterid =190399;        
+                var userid=0;        
+                var imagepage=1;        var imagecount=27;        var pagerrefresh=false;        var pagetype=2;        var postpageindex = 1;        var postpagecount = 0;       
+                var postcount=0;        var postsort=0;        var topicId=0;        var prechapterurl="/manga/shokugeki_no_soma/v01/c001/1.html";        
+                var nextchapterurl="/manga/shokugeki_no_soma/v01/c003/1.html";    
+        '''
+
+        p = re.compile('var comicid = (.*);')
+        for script in soup.find_all("script", {"src": False}):
+            if script:           
+                m = p.search(script.string)
+                if m:
+                    print(m)
+                    #(?<=var comicid = )[0-9]+(?=;)
+                    #(?<=var chapterid = )[0-9]+(?=;)
+                    #(?<=var imagecount = )[0-9]+(?=;)
+                    # https://github.com/Xonshiz/comic-dl/issues/196
+                #print(m.group(1))
 
 
     def pages(self):
@@ -83,6 +108,12 @@ class MangaFoxChapter(Chapter):
         last_page_number = max(page_numbers)
 
         print(self.source(''))
+
+        # http://fanfox.net/manga/gentleman_devil/v03/c038/chapterfun.ashx
+        '''
+        eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('r c(){2 h="//s.7.a/e/3/6/4-5.0/b";2 1=["/k.f?g=n&8=9","/l.f?g=m&8=9"];j(2 i=0;i<1.t;i++){u(i==0){1[i]="//s.7.a/e/3/6/4-5.0/b"+1[i];o}1[i]=h+1[i]}p 1}2 d;d=c();q=0;',31,31,'|pvalue|var|manga|01|038|22117|fanfox|ttl|1576252800|net|compressed|dm5imagefun||store|jpg|token|pix||for|e20191002_111418_525|e20191002_111418_526|b4e0963e2a1f126841f25f6aba6d2891f97edf70|8d4f90035f871134e742e2ca4daa7a7438cad7ce|continue|return|currentimageid|function||length|if'.split('|'),0,{}))
+
+        '''
 
         pages = []
         for i in range(first_page_number, last_page_number):
