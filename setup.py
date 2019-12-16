68d67f30ccd790a28f6855ecf2b9bd432831a54d
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-16 12:00:22 +0100

    better setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 """Setup for the manga package."""
 
-import setuptools
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
     README = f.read()
 
-setuptools.setup(
+setup(
     author="Alessandro Marzoli",
     author_email="alessandromarzoli@gmail.com",
     name='mangapy',
@@ -14,7 +14,7 @@ setuptools.setup(
     version='0.0.1',
     long_description=README,
     url='https://github.com/alemar11/manga',
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     python_requires=">=3.5",
     install_requires=['requests'],
     classifiers=[
