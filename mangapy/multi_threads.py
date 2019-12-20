90b89a97805c2aad773e1660c669fab4e538a728
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-20 14:16:53 +0100

    --wip-- [skip ci]

---
+++ mangapy/multi_threads.py
@@ -0,0 +1,56 @@
+import asyncio
+import random
+import time
+
+# https://github.com/python/cpython/blob/master/Doc/library/asyncio-queue.rst#id1
+# https://github.com/CaliDog/asyncpool
+# https://www.aeracode.org/2018/02/19/python-async-simplified/
+
+async def worker(name, queue):
+    while True:
+        # Get a "work item" out of the queue.
+        print(f'waiting {name}')
+        sleep_for = await queue.get()
+        print(f'ready {name}')
+
+        # Sleep for the "sleep_for" seconds.
+        await asyncio.sleep(sleep_for)
+
+        # Notify the queue that the "work item" has been processed.
+        queue.task_done()
+
+        print(f'{name} has slept for {sleep_for:.2f} seconds')
+
+
+async def main():
+    # Create a queue that we will use to store our "workload".
+    queue = asyncio.Queue()
+
+    # Generate random timings and put them into the queue.
+    total_sleep_time = 0
+    for _ in range(20):
+        sleep_for = random.uniform(0.05, 1.0)
+        total_sleep_time += sleep_for
+        queue.put_nowait(sleep_for)
+
+    # Create three worker tasks to process the queue concurrently.
+    tasks = []
+    for i in range(3):
+        task = asyncio.create_task(worker(f'worker-{i}', queue))
+        tasks.append(task)
+
+    # Wait until the queue is fully processed.
+    started_at = time.monotonic()
+    await queue.join()
+    total_slept_for = time.monotonic() - started_at
+
+    # Cancel our worker tasks.
+    #for task in tasks:
+    #    task.cancel()
+
+    print('====')
+    print(f'3 workers slept in parallel for {total_slept_for:.2f} seconds')
+    print(f'total expected sleep time: {total_sleep_time:.2f} seconds')
+
+
+asyncio.run(main())
