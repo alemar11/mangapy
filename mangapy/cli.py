db7a46b05b74b133be3c87a0fbebfd872f513013
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-24 12:22:38 +0100

    fix begin end download

--- mangapy/cli.py
+++ mangapy/cli.py
@@ -1,13 +1,14 @@
-import asyncio
 import sys
 import pkg_resources
 import argparse
 import os
 
+from mangapy.mangarepository import Chapter
 from mangapy.mangapark import MangaParkRepository
-from mangapy.mangarepository import download
+from threading import Thread, Semaphore
+from chapter_downloader import ChapterDownloader
 
-
+semaphore = Semaphore(3)
 version = pkg_resources.require("mangapy")[0].version
 
 
@@ -15,12 +16,12 @@ def cmdparse():
     """Returns parsed arguments from command line"""
     parser = argparse.ArgumentParser()
     parser.add_argument('title', type=str, help="manga title to download")
-   
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-a', '--all', action='store_true', help="download all chapters available")
     group.add_argument('-c', '--chapter', type=str, help="chapter(s) number to download")
     parser.add_argument('-d', '--dir', type=str, default='.', help='download directory')
-    parser.add_argument('-v', '--version', action='version',
+    parser.add_argument('-v', '--version',
+                        action='version',
                         version='{0} {1}'.format(parser.prog, version),
                         help="show program version and exit")
 
@@ -32,8 +33,8 @@ def cmdparse():
         chapter = args.chapter.split('-')
         if len(chapter) == 2:
             args.chapter = None
-            args.begin = chapter[0]
-            args.end = chapter[1] if chapter[1] else None
+            args.begin = int(chapter[0])
+            args.end = int(chapter[1]) if chapter[1] else None
         if args.begin and args.end and (int(args.begin) > int(args.end)):
             parser.print_usage()
             sys.exit("{0}: error: invalid chapter interval, the end "
@@ -49,14 +50,13 @@ def main():
     repository = MangaParkRepository()
     manga = repository.search(title)
     directory = os.path.join(directory, 'mangapark', manga.subdirectory)
-    tasks = []
+    chapters = []
 
     if manga is None or len(manga.chapters) <= 0:
         return
 
     if args.all:
-        for chapter in manga.chapters:
-            tasks.append(download(chapter, directory))
+        chapters = manga.chapters
     elif args.chapter:
         try:
             chapter_number = int(args.chapter.strip())
@@ -65,7 +65,7 @@ def main():
 
         for chapter in manga.chapters:
             if chapter.number == chapter_number:
-                tasks.append(download(chapter, directory))
+                chapters.append(chapter)
                 break
         else:
             sys.exit("Chapter doesn't exist.")
@@ -80,21 +80,28 @@ def main():
                 stop = index + 1
         for chapter in manga.chapters[start:stop]:
             # TODO: test
-            tasks.append(download(chapter, directory))
+            chapters.append(chapter)
 
     else:
         last_chapter = manga.chapters[-1]
-        tasks.append(download(last_chapter, directory))
+        chapters.append(last_chapter)
 
-    #loop = asyncio.new_event_loop()
-    #asyncio.set_event_loop(loop)
-    loop = asyncio.get_event_loop()
-    #https://www.educative.io/blog/python-concurrency-making-sense-of-asyncio 
-    loop.run_until_complete(asyncio.wait(tasks))
+    threads = [Thread(target=schedule, args=(chapter, directory)) for chapter in chapters]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+
+
+def schedule(chapter: Chapter, directory: str):
+    semaphore.acquire()
+    cd = ChapterDownloader()
+    cd.start(chapter=chapter, to=directory)
+    semaphore.release()
+
 
 if __name__ == '__main__':
     #sys.argv.insert(1, "Naruto - Eroi no Vol.1 (Doujinshi)")
     sys.argv.insert(1, "bleach")
     sys.argv.insert(2, "-d ~/Downloads/mangapy")
-    sys.argv.insert(2, "-c 2")
+    #sys.argv.insert(2, "-c 11")
+    sys.argv.insert(2, "-c 11-12")
     main()
