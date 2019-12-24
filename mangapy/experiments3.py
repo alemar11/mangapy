30a9a9f59bdd5988d2e0cdbe7e13a86ab9f57cd5
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-24 10:51:42 +0100

    working on threads

--- mangapy/experiments3.py
+++ mangapy/experiments3.py
@@ -8,78 +8,100 @@ from urllib.parse import urlparse
 from mangapy.mangarepository import Chapter
 from threading import Thread, current_thread
 
-class AsyncDownloader(object):
-    def __init__(self, concurrent_connections=100, silent=False):
+
+class MangaException(Exception):
+    """Exception class for manga"""
+    pass
+
+
+class ChapterDownloader(object):
+    def __init__(self, concurrent_connections=5, silent=False):
         self.concurrent_connections = concurrent_connections
         self.silent = silent
         self.progress_bar = None
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
-        self.semaphore = asyncio.Semaphore(concurrent_connections, loop=self.loop)
+        self.semaphore = asyncio.Semaphore(concurrent_connections, loop=self.loop)  
 
     def __del__(self):
         self.loop.close()
 
-    async def fetch(self, session, url):
+    async def fetch(self, session: ClientSession, url: str):
         async with self.semaphore:
-            async with session.get(url) as response:
-                task = asyncio.create_task(response.read())
-                resp = await task
+            try:
+                async with session.get(url) as response:
+                    if response.status != 200 and response.content is not None:
+                        return None
+                    else:
+                        content = await response.content.read()
+                        await response.release()
+                        if not self.silent:
+                            self.progress_bar.update(1)
+                        return content
+            except OSError as msg:
+                print(msg)
 
-                await response.release()
-                if not self.silent:
-                    self.progress_bar.update(1)
-                return resp
+    async def save(self, session: ClientSession, url: str, path: str, file_name: str):
+        file_ext = urlparse(url).path.split('.')[-1]
+        data = await self.fetch(session, url)
+        if data is None:
+            return
+        dir = os.path.expanduser(path)
+        if not os.path.isdir(path):
+            try:
+                os.makedirs(dir, exist_ok=True)
+            except OSError as msg:
+                raise MangaException(msg)
 
-    async def batch_download(self, urls, auth=None):
+        file = os.path.join(dir, file_name + '.' + file_ext)
+        output = open(file, "wb")
+        output.write(data)
+        output.close()
+
+    def start(self, chapter: Chapter, to: str):
+        self.loop.run_until_complete(self.download(chapter, to))
+
+    async def download(self, chapter: Chapter, to: str):
         async with ClientSession() as session:
-            await asyncio.gather(*[asyncio.create_task(self.download_and_save(session, url)) for url in urls])
+            to = os.path.join(to, str(chapter.number))
+            tasks = []
+            _pages = await chapter.pages()
+            self.progress_bar = tqdm(_pages, total=len(_pages), desc='Chapter {0}', unit='pages')
+            self.progress_bar.update(0)
 
-    async def download_and_save(self, session, url):
-        task = asyncio.ensure_future(self.fetch(session, url))
-        content = await task
+            for page in _pages:
+                url = page.url
+                tasks.append(self.save(session, url, to, str(page.number)))
 
-    def download(self, urls):
-        self.progress_bar = tqdm(urls, total=len(urls), desc='Chapter', unit='pages')
-        self.progress_bar.update(0)
-
-        tasks = self.batch_download(urls)
-        self.loop.run_until_complete(tasks)
-        
+            await asyncio.gather(*tasks)
 
 
 
 
-
-def do_stuff(i):
-
-    ad = AsyncDownloader(concurrent_connections=30)
-    data = ad.download([g for g in gen_url(upper=1000)])
+def do_stuff(c: Chapter):
+    cd = ChapterDownloader()
+    cd.start(c,'~/Downloads/__man')
+    #ad = AsyncDownloader(concurrent_connections=30)
+    #data = ad.download([g for g in gen_url(upper=1000)])
 
 
 
 def main():
-    num_threads = 3
-    threads = [Thread(target=do_stuff, args=(i,)) for i in range(num_threads)]
-    [thread.start() for thread in threads]
-    [thread.join() for thread in threads]
+    from mangapy.mangapark import MangaParkRepository
+    repository = MangaParkRepository()
+    manga = repository.search("naruto")
+    if manga is not None:
+        firstChapter = manga.chapters[0]
+        secondChapter = manga.chapters[1]
+        thirdChapter = manga.chapters[2]
+        lastChapter = manga.latest
+       
+        chapters = [firstChapter, secondChapter, thirdChapter, lastChapter]
 
-    threads = [Thread(target=do_stuff, args=(i,)) for i in range(num_threads)]
-    [thread.start() for thread in threads]
-    [thread.join() for thread in threads]
+        threads = [Thread(target=do_stuff, args=(c,)) for c in chapters]
+        [thread.start() for thread in threads]
+        [thread.join() for thread in threads]
 
-
-
-
-### call like so ###
-
-URL_PATTERN = 'https://www.example.com/{}.html'
-
-def gen_url(lower=0, upper=None):
-    for i in range(lower, upper):
-        yield URL_PATTERN.format(i)   
 
 if __name__ == "__main__":
     main()
-
-   
