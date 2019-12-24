80090462e6dd1b3d30584f0d6b887a3e7154f457
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-24 10:13:25 +0100

    threads

--- mangapy/experiments3.py
+++ mangapy/experiments3.py
@@ -6,15 +6,19 @@ import os
 from PIL import Image
 from urllib.parse import urlparse
 from mangapy.mangarepository import Chapter
-
+from threading import Thread, current_thread
 
 class AsyncDownloader(object):
     def __init__(self, concurrent_connections=100, silent=False):
         self.concurrent_connections = concurrent_connections
         self.silent = silent
         self.progress_bar = None
-        self.loop = asyncio.get_event_loop()
-        self.semaphore = asyncio.Semaphore(concurrent_connections)
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.semaphore = asyncio.Semaphore(concurrent_connections, loop=self.loop)
+
+    def __del__(self):
+        self.loop.close()
 
     async def fetch(self, session, url):
         async with self.semaphore:
@@ -41,6 +45,30 @@ class AsyncDownloader(object):
 
         tasks = self.batch_download(urls)
         self.loop.run_until_complete(tasks)
+        
+
+
+
+
+
+def do_stuff(i):
+
+    ad = AsyncDownloader(concurrent_connections=30)
+    data = ad.download([g for g in gen_url(upper=1000)])
+
+
+
+def main():
+    num_threads = 3
+    threads = [Thread(target=do_stuff, args=(i,)) for i in range(num_threads)]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+
+    threads = [Thread(target=do_stuff, args=(i,)) for i in range(num_threads)]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+
+
 
 
 ### call like so ###
@@ -52,13 +80,6 @@ def gen_url(lower=0, upper=None):
         yield URL_PATTERN.format(i)   
 
 if __name__ == "__main__":
-    from mangapy.mangapark import MangaParkRepository
+    main()
 
-    #repository = MangaParkRepository()
-    #manga = repository.search("naruto")
-
-    from tqdm import trange
-    from time import sleep
-
-    ad = AsyncDownloader(concurrent_connections=30)
-    data = ad.download([g for g in gen_url(upper=1000)])
+   
