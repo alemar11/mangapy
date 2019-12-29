d20de6cab8e3705cf6d16dabe43f9156ade563ce
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-29 21:32:33 +0100

    refactor

--- tests/context.py
+++ tests/context.py
@@ -2,11 +2,8 @@ import mangapy  # noqa: F401
 import os
 import sys
 
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 # https://docs.python-guide.org/writing/structure/
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # https://hidemy.name/en/proxy-list/?type=hs#list
 test_proxies = {'http': '5.135.182.93:3128', 'https': '5.135.182.93:3128'}
