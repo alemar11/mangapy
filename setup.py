e69918b569ec3d23de23a6f4825199a93119c9a7
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-12-29 19:00:45 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/alemar11/manga',
     packages=find_packages(),
     python_requires=">=3.7",
-    install_requires=['pillow', 'requests', 'tqdm'],
+    install_requires=['bs4', 'pillow', 'requests', 'tqdm'],
     classifiers=[
         # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
         'Development Status :: 5 - Production/Stable',
